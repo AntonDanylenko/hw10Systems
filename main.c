ee6678643a10ab37183cb6b989e63f7398580d81
Anton Danylenko <adanylenko@stuy.edu>
2018-10-28 22:18:08 -0400

    s

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int getRandom(){
     printf("errno %d: %s\n", errno, strerror(errno));
   }
   close(fd);
-  printf("answer: %d\n", answer);
+  //printf("answer: %d\n", answer);
   return answer;
 }
 
@@ -25,6 +25,19 @@ int main(){
     printf("  random %d: %d\n", x, randsInitial[x]);
   }
 
+  printf("Writing numbers to file...");
+  int fd = open("temp.txt", O_RDWR);
+  write(fd, randsInitial, sizeof(randsInitial));
+
+  printf("Reading numbers from file...");
+  int randsFinal[10];
+  read(fd, randsFinal, sizeof(randsFinal));
+  close(fd);
+  
+  printf("Verification that written values were the same:\n")
+  for (int x=0; x<10; x++){
+    printf("  random %d: %d\n", x, randsFinal[x]);
+  }
   /*printf("write wassup\n");
   write(fd, "wassup\n", 7);
 
