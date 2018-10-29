668346e91497076f2ef8dec62d5eafb9adca6efa
Anton Danylenko <adanylenko@stuy.edu>
2018-10-28 21:58:56 -0400

    s

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int getRandom(){
+  int fd = open("/dev/random", O_RDONLY);
+  int answer;
+  read(fd, &answer, sizeof(int));
+  if (errno) {
+    printf("errno %d: %s\n", errno, strerror(errno));
+  }
+  close(fd);
+  printf("answer: %d\n", answer);
+  return answer;
+}
+
+int main(){
+  printf("Generating random numbers:\n");
+  int randsInitial[10];
+  for (int x=0; x<10; x++){
+    randsInitial[x] = getRandom();
+    printf("  random %d: %d\n", x, randsInitial[x]);
+  }
+
+  /*printf("write wassup\n");
+  write(fd, "wassup\n", 7);
+
+  close(fd);
+  printf("closed fd\n");
+
+  printf("opened fd, can read\n");
+  fd = open("file.txt", O_RDONLY);
+
+  printf("reading\n");
+  read(fd, b, 7);
+  b[7] = "\0";
+
+  printf("closed fd\n");
+  close(fd);
+
+  printf("should print wassup: %s\n", b);*/
+}
